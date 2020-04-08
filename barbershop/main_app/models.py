89299b59160e7e3402a0b526f0340042660d8c1d
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-04-08 14:15:18 -0400

    django-model

--- barbershop/main_app/models.py
+++ barbershop/main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Barber(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    experience = models.IntegerField()
+    description = models.TextField(max_length=250)
+    def __str__(self):
+        return self.name
--- barbershop/main_app/urls.py
+++ barbershop/main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('barbers/', views.barbers_index, name='index')
+    path('barbers/', views.barbers_index, name='index'),
+    path('barbers/<int:barber_id>', views.barbers_detail, name='detail'),
 ]
